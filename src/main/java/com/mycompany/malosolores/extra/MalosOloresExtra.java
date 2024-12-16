f57cbab07ff9fb18900a3c4d8c92b48fc639d520
marciatorresg <marciatorresg@gmail.com>
2024-12-16 16:27:10 -0500

    Creación del repositorio

---
+++ src/main/java/com/mycompany/malosolores/extra/MalosOloresExtra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.malosolores.extra;
+
+/**
+ *
+ * @author User
+ */
+public class MalosOloresExtra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
