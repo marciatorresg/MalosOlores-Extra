54ae2ea017e983701f23bc079a0bc21616fa3c59
Juan Carlos Romero Uchubanda <152119104+jcarrome@users.noreply.github.com>
2024-12-17 21:41:03 -0500

    Update Usuario.java

--- src/main/java/com/mycompany/malosolores/extra/Usuario.java
+++ src/main/java/com/mycompany/malosolores/extra/Usuario.java
@@ -1,6 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ *Código basado en el concepto de "Bad Smells" en código.
+ *Fuente: "Bad Smells" en código, M.C. Juan Carlos Olivares Rojas, Febrero 2011.
+ *https://www.slideserve.com/byron-mayo/bad-smells-en-c-digo
  */
 package com.mycompany.malosolores.extra;
 
